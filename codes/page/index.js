58649b0f90c79656883c9eeff28c988b8b81c571
sou31415 <tbc3141@icloud.com>
2023-05-15 13:30:22 +0900

    fix html

--- codes/page/index.js
+++ codes/page/index.js
@@ -22,3 +22,20 @@ $(window).on('load', function(){
     $(Box).slideDown(500);          //アコーディオンを開く
   });
 });
+
+function set2fig(num) {
+   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+   var ret;
+   if( num < 10 ) { ret = "0" + num; }
+   else { ret = num; }
+   return ret;
+}
+function showClock2() {
+   var nowTime = new Date();
+   var nowHour = set2fig( nowTime.getHours() );
+   var nowMin  = set2fig( nowTime.getMinutes() );
+   var nowSec  = set2fig( nowTime.getSeconds() );
+   var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+   document.getElementById("RealtimeClockArea2").innerHTML = msg;
+}
+setInterval('showClock2()',1000);
