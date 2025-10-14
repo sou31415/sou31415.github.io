64e30a11b521c4d8d83dda452cafbf83c1a21c98
sou31415 <tbc3141@icloud.com>
2025-10-15 01:08:14 +0900

    change root

---
+++ docs/doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"rustlib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"rustlib/modint/struct.Modint.html\" title=\"struct rustlib::modint::Modint\">Modint</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
